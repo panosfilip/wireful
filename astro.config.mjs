9da15f7ab394fabca664c5bedc80fbbbd3f7d096
Panagiotis Filippaios <panagiotis@Panagiotiss-Mac-mini.local>
2026-07-09 23:08:16 +0300

    Correct GitHub Pages site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://panosfilip.github.io/wireful",
+  site: "https://panosfilip.github.io",
   base: "/wireful",
 });
